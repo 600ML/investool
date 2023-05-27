7b0a3c96fda5a36eeddcef30b2179a6fa0a0049f
方航 <fanghang@tange.ai>
2023-05-27 21:10:09 +0800

    调整路由

--- routes/routes.go
+++ routes/routes.go
@@ -6,16 +6,16 @@ import "github.com/gin-gonic/gin"
 
 // Routes 注册 API URL 路由
 func Routes(app *gin.Engine) {
-	app.GET("/invest/", StockIndex)
-	app.POST("/invest/selector", StockSelector)
-	app.POST("/invest/checker", StockChecker)
-	app.GET("/invest/fund", FundIndex)
-	app.GET("/invest/fund/filter", FundFilter)
-	app.POST("/invest/fund/check", FundCheck)
-	app.GET("/invest/about", About)
-	app.GET("/invest/comment", Comment)
-	app.GET("/invest/fund/similarity", FundSimilarity)
-	app.GET("/invest/materials", Materials)
-	app.POST("/invest/fund/query_by_stock", QueryFundByStock)
-	app.GET("/invest/fund/managers", FundManagers)
+	app.GET("/", StockIndex)
+	app.POST("/selector", StockSelector)
+	app.POST("/checker", StockChecker)
+	app.GET("/fund", FundIndex)
+	app.GET("/fund/filter", FundFilter)
+	app.POST("/fund/check", FundCheck)
+	app.GET("/about", About)
+	app.GET("/comment", Comment)
+	app.GET("/fund/similarity", FundSimilarity)
+	app.GET("/materials", Materials)
+	app.POST("/fund/query_by_stock", QueryFundByStock)
+	app.GET("/fund/managers", FundManagers)
 }
